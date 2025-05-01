f0add34849ca33b2dfdf3fff747f9058968c85f6
Chondromatidis Eleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2025-05-01 18:46:07 +0300

    Update and rename hash_integrity.py to Ask22874.py

---
+++ Ask22874.py
@@ -0,0 +1,15 @@
+def verify_file_integrity(original_hash, current_hash):
+    try:
+        with open(original_hash, 'r') as file:
+            original_hash = file.read()  # Διαβάζει όλα τα περιεχόμενα και τα αποθηκεύει σε μεταβλητή
+            print("Το περιεχόμενο του αρχείου είναι:", original_hash)
+    except FileNotFoundError:
+        print("Το αρχείο δεν βρέθηκε!")
+    if original_hash and current_hash:
+        original_hash = original_hash.strip()
+        current_hash = current_hash.strip()
+        if current_hash == original_hash:
+            return True
+        return False
+    else:
+        return False
