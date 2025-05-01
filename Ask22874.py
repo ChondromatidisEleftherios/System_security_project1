358697f732efbd936f6c2e7fadd196077c93ffd9
Chondromatidis Eleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2025-05-01 18:52:04 +0300

    Update Ask22874.py

--- Ask22874.py
+++ Ask22874.py
@@ -1,7 +1,7 @@
 def verify_file_integrity(original_hash, current_hash):
     try:
         with open(original_hash, 'r') as file:
-            original_hash = file.read()  # Διαβάζει όλα τα περιεχόμενα και τα αποθηκεύει σε μεταβλητή
+            original_hash = file.read() 
             print("Το περιεχόμενο του αρχείου είναι:", original_hash)
     except FileNotFoundError:
         print("Το αρχείο δεν βρέθηκε!")
